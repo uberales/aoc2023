693281f829b43df61494fccb1f08b2a2a0170eac
Aleš Podolník <ales.podolnik@gmail.com>
2023-12-05 20:45:21 +0100

    Add part 2 of day 5

--- 05/day05.py
+++ 05/day05.py
@@ -39,8 +39,7 @@ with open('input.txt', mode='r') as f:
 def resolve(map_name, i):
     for d0, s0, l in map_def[map_name]:
         if i >= s0 and i < s0 + l:
-            d = i - s0
-            return d0 + d
+            return d0 + i - s0
     return i        
 
 locations = []
@@ -54,8 +53,59 @@ print(min(locations))
 
 # part 2
 
-# tbd
 
-for i in range(0, len(seeds), 2):
-    print(seeds[i+1])
+seed_ranges = [(seeds[i], seeds[i]+seeds[i+1] - 1) for i in range(0, len(seeds), 2)]
+
+def split(rng_s, rng_u):
+    to_map = []
+    unmapped = []
+    
+    if rng_u[1] < rng_s[0]:
+        unmapped.append(rng_u)
+    elif rng_u[0] < rng_s[0] and rng_u[1] <= rng_s[1]:
+        unmapped.append((rng_u[0], rng_s[0] - 1))
+        to_map.append((rng_s[0], rng_u[1]))
+    elif rng_u[0] < rng_s[0] and rng_u[1] > rng_s[1]:
+        unmapped.append((rng_u[0], rng_s[0] - 1))
+        to_map.append(rng_s)
+        unmapped.append((rng_s[1]+1, rng_u[1]))
+    elif rng_u[0] >= rng_s[0] and rng_u[1] <= rng_s[1]:
+        to_map.append(rng_u)
+    elif rng_u[0] < rng_s[1] and rng_u[1] > rng_s[1]:
+        to_map.append((rng_u[0], rng_s[1]))
+        unmapped.append((rng_s[1]+1, rng_u[1]))
+    elif rng_u[0] > rng_s[1]:
+        unmapped.append(rng_u)
         
+    return to_map, unmapped
+
+def resolve_ranges(map_name, range_list):
+    def map_range(r, d0, s0):
+        return (d0 + r[0] - s0, d0 + r[1] - s0)
+    
+    mapped = []
+    unmapped = range_list
+    for d0, s0, l in map_def[map_name]:
+        rng_s = (s0, s0 + l - 1)
+        um_next = []
+        for rng_u in unmapped:
+            to_map, um_part = split(rng_s, rng_u)
+            um_next.extend(um_part)
+            
+            for rng in to_map:
+                rng_1 = map_range(rng, d0, s0)
+                mapped.append(rng_1)
+            
+        unmapped = um_next
+    
+    return [*mapped, *unmapped]
+
+loc_ranges = []
+for sr in seed_ranges:
+    rl = [sr]
+    for mn in map_names:
+        rl = resolve_ranges(mn, rl)
+    loc_ranges.extend(rl)
+    
+print(min([r[0] for r in loc_ranges]))
+
