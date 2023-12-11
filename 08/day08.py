3dda90077fd00766893e7a5a041e085e80c7b9fb
Aleš Podolník <ales@podolnik.me>
2023-12-11 12:18:02 +0100

    Add solution of day 8, part 2

--- 08/day08.py
+++ 08/day08.py
@@ -7,8 +7,9 @@ Created on Fri Dec  8 09:58:41 2023
 """
 
 import re
+import numpy as np
 
-with open('test.txt', mode='r') as f:
+with open('input.txt', mode='r') as f:
     instructions = list(f.readline().strip())
     txt = f.read()
     matches = re.findall('([0-9A-Z]+)\s=\s\(([0-9A-Z]+), ([0-9A-Z]+)\)', txt)
@@ -23,7 +24,6 @@ def iterate(n_from, n_to, offset=0):
         d = instructions[i % n_i]
         n = nodes[n][d]
         i += 1
-        print(n, i)
         if n.endswith(n_to):
             break
     return i-offset, n
@@ -36,31 +36,8 @@ print(i)
 
 # part 2
 
-def check_loop(n_from):
-    n = n_from
-    i = 0
-    seen = {(n, 0): 0}
-    while True:
-        d = instructions[i % n_i]
-        n = nodes[n][d]
-        i += 1
-        if (n, i % n_i) in seen:
-            break
-        seen[(n, i % n_i)] = i
-    return n, seen[(n, i % n_i)], i
-
 nodes_start = [n for n in nodes if n.endswith('A')]
-nodes_loops = {n: check_loop(n) for n in nodes_start}
-nodes_periods = {n: iterate(nodes_loops[n][0], 'Z', offset=nodes_loops[n][1])[0] for n in nodes_start}
 nodes_end = {n: iterate(n, 'Z') for n in nodes_start}
 
-min_vals = [nodes_loops[n][1] for n in nodes_start]
-deltas = [nodes_periods[n] for n in nodes_periods]
-
-i = 0
-while True:
-    pos = [min_vals[k] + i * d for k, d in enumerate(deltas)]
-    if len(set(pos)) == 1 and i > 0:
-        break
-    i += 1
-print(i)
+nodes_periods = [nodes_end[n][0] for n in nodes_end]
+print(np.lcm.reduce(nodes_periods))
